d7e72a55e183873f87eaf01ba49750d13b4b1165
Sulabh Shrestha <Shresthasulabh777@gmail.com>
2023-08-10 17:04:57 +0545

    Created custom logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="bg-green-300 py-16">
+      <div className="tamara-logo mx-auto relative bg-red-500 w-[80px] h-[100px] block rounded-[50%/50%] -rotate-45">
+        <div className="absolute bg-amber-100 w-[70px] h-[90px] rounded-[50%/50%] rotate-17 bottom-0 left-7"></div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
